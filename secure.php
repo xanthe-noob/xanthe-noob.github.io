312c5b5af647725206e566a49847318307091081
xanthe-noob <92683255+xanthe-noob@users.noreply.github.com>
2021-10-31 15:07:33 +0100

    Create secure.php

---
+++ secure.php
@@ -0,0 +1,104 @@
+<!-- COPYLEFT portail.ml -->
+<html>
+<head>
+<meta charset="utf-8" />
+	<title>portail.ml</title>
+	<style>
+		a:link {color:black; text-decoration:none;}
+		a:visited {color:black; text-decoration:none;}
+		a:hover {color:#E74C3C; text-decoration:none;}
+		a:active {color:black; text-decoration:none;}
+		p.p1 {text-align:left; font-family:arial;}
+		div.arial {font-family: arial; font-size: 30px;}
+		div.text {font-family: arial; font-size: 15px;}
+		div.cgu {text-align:justify; color:gray; font-family:arial; font-size:10px;}
+		hr {border-width: 1px;}
+		table.main {border-spacing: 20px;}
+</style>
+<meta name="viewport" content="width=device-width"/>
+<link rel="icon" href="favicon.ico" />
+</head>
+
+<body bgcolor="blue">
+
+<?php
+ if (isset($_POST['mot_de_passe']) AND $_POST['mot_de_passe'] == "elixire") {
+ ?>
+		<table class="main" border=0 width=100% height=100% align=center bgcolor=white>
+		<tr>
+			<td>
+
+	<table border=0 align=left bgcolor=white>
+		<tr>
+			<td width=80%>
+				<div class="arial"><img src="https://static.s-sfr.fr/media/v2/assets/images/png/sfr-logo.png" border=0 height=30px /> Portail privé <font size=1>v2.0.1</font></div>
+			<hr color=blue />
+				<p class="p1"><a href="https://www.sfr.fr/cas/login?service=https%3A%2F%2Fwww.red-by-sfr.fr%2Faccueil%2Fj_spring_cas_security_check&theme=espaceclientred#redclicid=X_Menu_EspaceClient" target="_blank">&#10003; <font face="arial">Connexion</font></a></p>
+			<hr color=blue />
+				<p class="p1"><a href="https://www.red-by-sfr.fr/mon-espace-client/parrainage-proxy.html?codeOption=GCO_15E_RED&line=0231081987&idParrain=RED4F86B0C&universe=RED&environment=FIXE&pointsConsommes=15" target="_blank">&#9921; <font face="arial">Déclencher -15€ sur 02.31.08.19.87 - Sylvie M.</font></a>
+				<br /><i><font color=gray>04</font></i><br />&nbsp;<br />
+        <a href="https://www.red-by-sfr.fr/mon-espace-client/parrainage-proxy.html?codeOption=GCO_15E_RED&line=0250109832&idParrain=REDE6139B3&universe=RED&environment=FIXE&pointsConsommes=15" target="_blank">&#9921; <font face="arial">Déclencher -15€ sur 02.50.10.98.32 - Charlie C.</font></a>
+				<br /><i><font color=gray>06</font></i><br />&nbsp;<br />
+        <a href="https://www.red-by-sfr.fr/mon-espace-client/parrainage-proxy.html?codeOption=SGG1&line=0679813382&idParrain=RED8DE1CB9&universe=RED&environment=FIXE&pointsConsommes=10" target="_blank">&#9921; <font face="arial">Déclencher -10€ sur 06.79.81.33.82 - Sylvie M.</font></a>
+				<br /><i><font color=gray>10</font></i><br />&nbsp;<br />
+        <a href="https://www.red-by-sfr.fr/mon-espace-client/parrainage-proxy.html?codeOption=GCO_15E_RED&line=0679813382&idParrain=RED8DE1CB9&universe=RED&environment=FIXE&pointsConsommes=10" target="_blank">&#9921; <font face="arial">Déclencher -10€ sur 0</font></a>
+				<br /><i><font color=gray>04</font></i><br />&nbsp;<br />
+        <a href="https://www.red-by-sfr.fr/mon-espace-client/parrainage-proxy.html?codeOption=GCO_15E_RED&line=0231081987&idParrain=RED4F86B0C&universe=RED&environment=FIXE&pointsConsommes=15" target="_blank">&#9921; <font face="arial">Déclencher -15€ sur 02.31.08.19.87</font></a>
+				<br /><i><font color=gray>04</font></i><br />&nbsp;<br />
+        <a href="https://www.red-by-sfr.fr/mon-espace-client/parrainage-proxy.html?codeOption=GCO_15E_RED&line=0231081987&idParrain=RED4F86B0C&universe=RED&environment=FIXE&pointsConsommes=15" target="_blank">&#9921; <font face="arial">Déclencher -15€ sur 02.31.08.19.87</font></a>
+				<br /><i><font color=gray>04</font></i><br />&nbsp;<br />
+        <a href="https://www.red-by-sfr.fr/mon-espace-client/parrainage-proxy.html?codeOption=GCO_15E_RED&line=0231081987&idParrain=RED4F86B0C&universe=RED&environment=FIXE&pointsConsommes=15" target="_blank">&#9921; <font face="arial">Déclencher -15€ sur 02.31.08.19.87</font></a>
+				<br /><i><font color=gray>04</font></i><br />&nbsp;<br />
+        <a href="https://www.red-by-sfr.fr/mon-espace-client/parrainage-proxy.html?codeOption=GCO_15E_RED&line=0231081987&idParrain=RED4F86B0C&universe=RED&environment=FIXE&pointsConsommes=15" target="_blank">&#9921; <font face="arial">Déclencher -15€ sur 02.31.08.19.87</font></a>
+				<br /><i><font color=gray>04</font></i><br />&nbsp;<br />
+        <a href="https://www.red-by-sfr.fr/mon-espace-client/parrainage-proxy.html?codeOption=GCO_15E_RED&line=0231081987&idParrain=RED4F86B0C&universe=RED&environment=FIXE&pointsConsommes=15" target="_blank">&#9921; <font face="arial">Déclencher -15€ sur 02.31.08.19.87</font></a>
+        <br /><i><font color=gray>04</font></i><br />&nbsp;<br />
+        </p>
+		</td>
+		</tr>
+	</table>
+<p>&nbsp;</p>
+	<table align=right border=0>
+		<tr>
+			<td>
+			<div align=right><font size=2 color=gray face="arial"><i>copyleft <font size=2 color=gray face="arial">net333</font></font></i></font></div>
+			</td>
+		</tr>
+	</table>
+			</td>
+		</tr>
+	</table>
+
+<?php
+ }
+ else { echo '
+<table class="main" border=0 width=100% height=100% align=center bgcolor=white>
+		<tr>
+			<td>
+
+	<table border=0 align=left bgcolor=white>
+		<tr>
+			<td width=80%>
+				<div class="arial">Portail.ml <font size=1>v1.0.1</font><br />&nbsp;</div>
+				<div class="text"><b><font color=red>Mot de passe erroné !</font></b>
+				<br />Si vous n&apos;êtes pas autorisé à accéder à cette page, merci de ne pas réessayer.<br />&nbsp;<br />
+				<form action="secure.php" method="post">
+				<input type="password" name="mot_de_passe" id="mot_de_passe"/>
+				<input type="submit" value="Valider" />
+				</form><i><font size=1 color=gray>(code pdv)</font></i>
+				</div>
+	</table>
+<p>&nbsp;</p>
+	<table align=right border=0>
+		<tr>
+			<td>
+			<div align=right><font size=2 color=gray face="arial"><i>copyleft <font size=2 color=gray face="arial">portail.ml</font></font></i></font></div>
+			</td>
+		</tr>
+	</table>
+			</td>
+		</tr>
+	</table>';
+ } ?>
+</body>
+</html>
